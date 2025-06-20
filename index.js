89601f8062e7ea3460d71d70cb7cbe0f14f24fec
ethandoes-code <ethandoescode@gmail.com>
2025-06-20 15:05:46 -0500

    (dev): Add functionality to display top funded game and runner up using destructuring and the spread operator

--- index.js
+++ index.js
@@ -182,7 +182,14 @@ const sortedGames =  GAMES_JSON.sort( (item1, item2) => {
 });
 
 // use destructuring and the spread operator to grab the first and second games
+const [topPledge, runnerUpPledge, ...rest] = sortedGames;
 
 // create a new element to hold the name of the top pledge game, then append it to the correct element
+const topPledgeName = document.createElement("p");
+topPledgeName.innerText = topPledge.name;
+firstGameContainer.appendChild(topPledgeName);
 
 // do the same for the runner up item
+const runnerUpPledgeName = document.createElement("p");
+runnerUpPledgeName.innerText = runnerUpPledge.name;
+secondGameContainer.appendChild(runnerUpPledgeName);
